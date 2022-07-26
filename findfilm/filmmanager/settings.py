cb2158e2ab1ae5214868576679ace8bb683b5bfc
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-07-25 23:21:33 -0700

    .

--- findfilm/filmmanager/settings.py
+++ findfilm/filmmanager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'findfilm.frontend',
+    'frontend',
     'preferences',
     'accounts',
     'whitenoise.runserver_nostatic',
