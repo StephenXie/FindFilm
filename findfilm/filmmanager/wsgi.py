1ad09b3f75454ce15fd1108db40dbd5298dd1f12
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-07-25 20:24:51 -0700

    .

--- findfilm/filmmanager/wsgi.py
+++ findfilm/filmmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findfilm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findfilm.filmmanager.settings')
 
 application = get_wsgi_application()
